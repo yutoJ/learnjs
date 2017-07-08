34095abd3db2fa103146c20d04c2798ac40ea1a7
yuto <vagrant@vagrant-ubuntu-trusty.vagrantup.com>
2017-07-08 03:54:12 +0000

    sec2

--- public/tests/SpecHelper.js
+++ public/tests/SpecHelper.js
@@ -1,6 +1,6 @@
 var fixture;
 
-function loadFixture(path) {  
+function loadFixture(path) {
   var html;
   jQuery.ajax({
     url: '/index.html',
@@ -8,7 +8,7 @@ function loadFixture(path) {
       html = result;
     },
     async: false
-  });          
+  });
   return $.parseHTML(html);
 }
 
